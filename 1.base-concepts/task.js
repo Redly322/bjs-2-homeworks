4a27b8d553bc65a06506d1507e547f0a599d1bf0
Ilya Zakharov <redly62@gmail.com>
2024-02-02 11:57:16 +0300

    Задачи выполнены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,22 +2,24 @@
 
 function solveEquation(a, b, c) {
 	let arr = [];
-	let d = (Math.pow(b, 2)) - (4 * a * c);
-	if (d < 0) {
-		arr = [];
-	} else if (d == 0) {
-		arr[0] = (-b / (2 * a));
-	} else if (d > 0) {
-		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
-		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
-	}
-	return arr;
+ let d = (Math.pow(b, 2)) - (4 * a * c);
+  if (d < 0) {
+    arr = [];
+  }
+  else if (d == 0) {
+    arr[0] = (-b / (2 * a));
+  }
+  else if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let P = (percent / 100) / 12;
-	let S = amount - contribution;
-	let payment = S * (P + (P / ((Math.pow((1 + P), countMonths)) - 1)));
-	let totalAmount = (payment * countMonths).toFixed(2);
-	return Number(totalAmount);
+	let P = (percent / 10) / 12;
+  let S = amount - contribution;
+  let payment = S * (P + (P / (((1 + P) ** countMonths) - 1)));
+  let totalAmount = (payment * countMonths).toFixed(2);
+  return totalAmount;
 }
