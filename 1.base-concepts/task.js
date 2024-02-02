232967b15cca4649c1a299131be594d573ec0e5e
Ilya Zakharov <redly62@gmail.com>
2024-02-02 12:00:04 +0300

    Задачи выполнены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,17 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-	let arr = [];
- let d = (Math.pow(b, 2)) - (4 * a * c);
-  if (d < 0) {
-    arr = [];
-  }
-  else if (d == 0) {
-    arr[0] = (-b / (2 * a));
-  }
-  else if (d > 0) {
-    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
-    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
-  }
-  return arr;
+  let arr = [];
+	let d = (Math.pow(b, 2)) - (4 * a * c);
+	if (d < 0) {
+		arr = [];
+	} else if (d == 0) {
+		arr[0] = (-b / (2 * a));
+	} else if (d > 0) {
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
