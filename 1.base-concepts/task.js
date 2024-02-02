038b4b2abfde2b01e4701600212c2d9f7b306040
Ilya Zakharov <redly62@gmail.com>
2024-02-02 12:04:46 +0300

    Задачи выполнены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,12 +2,12 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-	let d = (Math.pow(b, 2)) - (4 * a * c);
-	if (d < 0) {
+	let diskr = (Math.pow(b, 2)) - (4 * a * c);
+	if (diskr < 0) {
 		arr = [];
-	} else if (d == 0) {
+	} else if (diskr == 0) {
 		arr[0] = (-b / (2 * a));
-	} else if (d > 0) {
+	} else if (diskr > 0) {
 		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
 		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
 	}
