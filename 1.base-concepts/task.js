bea469fd69caecec5f8d8e4edb819b80a6193e79
Ilya Zakharov <redly62@gmail.com>
2024-02-02 11:53:55 +0300

    Задачи выполнены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -8,8 +8,8 @@ function solveEquation(a, b, c) {
 	} else if (diskr == 0) {
 		arr.push(-b / (2 * a));
 	} else {
-		arr.push(-b + Math.sqrt(d)) / (2 * a);
-		arr.push(-b - Math.sqrt(d)) / (2 * a);
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
 	}
 	return arr;
 }
